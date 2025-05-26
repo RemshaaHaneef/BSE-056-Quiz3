be57b19cd1b3a54d622bc8ca2241e80c54724a71
RemshaaHaneef <remshahaneef0@gmail.com>
2025-05-25 23:03:41 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,17 +2,5 @@ python
 print("Hello from Python!")
 
 
-* Add a *Jenkinsfile*:
 
-groovy
-pipeline {
-    agent any
-    stages {
-        stage('Run Python Script') {
-            steps {
-                sh 'python3 hello.py'
-            }
-        }
-    }
-}
 
