93797a43d6c1b4ff2d79e3993a5f2bff6aa2709a
RemshaaHaneef <remshahaneef0@gmail.com>
2025-05-25 22:24:43 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+python
+print("Hello from Python!")
+
+
+* Add a *Jenkinsfile*:
+
+groovy
+pipeline {
+    agent any
+    stages {
+        stage('Run Python Script') {
+            steps {
+                sh 'python3 hello.py'
+            }
+        }
+    }
+}
+
